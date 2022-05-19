bd0c86edee87e624a9ba283fed85aea11f01fea8
Shubhyaa <105843073+Shubhyaa@users.noreply.github.com>
2022-05-19 12:11:36 +0530

    Add files via upload

---
+++ Practical/CharDataType.java
@@ -0,0 +1,16 @@
+package Joshi;
+
+public class CharDataType {
+	public static void main(String args[])
+	{
+	char marks, grade; 
+	
+	marks = '8';
+	grade = 'B';
+	
+	System.out.println("Marks: "+marks);
+	System.out.println("Grade: "+grade);
+	}
+	}
+
+
