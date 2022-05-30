3152ba2323d22a1e5bcd5023f31545c437af0265
Shubhyaa <105843073+Shubhyaa@users.noreply.github.com>
2022-05-30 13:10:02 +0530

    Add files via upload

---
+++ RelationOperator.java
@@ -0,0 +1,20 @@
+package Joshi;
+
+public class RelationOperator {
+
+public static void main(String[] args) {
+		
+		
+		int num1 = 12, num2 = 4;
+		
+		System.out.println("num1 == num2 = " + (num1 == num2) );
+		System.out.println("num1 != num2 = " + (num1 != num2) );
+		System.out.println("num1 >  num2 = " + (num1 >  num2) );
+		System.out.println("num1 <  num2 = " + (num1 <  num2) );
+		System.out.println("num1 >= num2 = " + (num1 >= num2) );
+		System.out.println("num1 <= num2 = " + (num1 <= num2) );
+
+	}
+
+
+}
